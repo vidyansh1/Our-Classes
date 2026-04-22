6267d54357e7ddac0b6c7e77aa7dac1eaef7fa9c
vidyansh1 <vidyanshyadav15@gmail.com>
2026-04-22 12:50:10 +0530

    first commit

--- site-config.js
+++ site-config.js
@@ -1,13 +1,13 @@
 window.OWNCLASSES_CONFIG = {
   apiBaseUrl: "http://localhost:5000/api",
-  brandName: "Own Classes",
+  brandName: "Our Classes",
   tagline: "Learn in your own way with Dedicated Mentorship",
   founderName: "Rameez Mirza Sir",
   contactEmail: "ownclassesofficial@gmail.com",
   contactPhone: "+91 92175 84525",
-  logoUrl: "https://ownclasses.com/wp-content/uploads/2026/01/cropped-Gemini_Generated_Image_1tx4ls1tx4ls1tx4-1.jpg",
-  founderImageUrl: "https://ownclasses.com/wp-content/uploads/2026/01/WhatsApp-Image-2026-01-11-at-9.15.33-PM-Edited-768x1024.jpg",
-  heroImageUrl: "https://ownclasses.com/wp-content/uploads/2026/01/upsc_mentorship_1800x300_kwy30xx7.jpg",
+  logoUrl: "public/logo-our-classes.png",
+  founderImageUrl: "public/Rameez.png",
+  heroImageUrl: "public/upsc_mentorship_1800x300_kwy30xx7",
   liveClass: {
     joinUrl: "",
     notesUrl: ""
